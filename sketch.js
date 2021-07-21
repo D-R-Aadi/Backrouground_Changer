eb24c58f0e4dfb5cd2110aeb8065d7ffc5c129dd
D-R-Aadi <86865270+D-R-Aadi@users.noreply.github.com>
2021-07-21 10:34:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+function setup() {
+  createCanvas(1600,984);
+}
+
+function draw() 
+{
+  background("yellow");
+
+if(keyDown(UP_ARROW))
+{
+  background("red")
+}
+
+if(keyDown(DOWN_ARROW))
+{
+  background("blue")
+}
+
+if(keyDown(LEFT_ARROW))
+{
+  background("green")
+}
+if(keyDown(RIGHT_ARROW))
+{
+  background("black")
+}
+
+
+}
+
+
+
+
